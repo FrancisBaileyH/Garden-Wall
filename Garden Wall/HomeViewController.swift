2cd9da38dec3d0fbfc4b8a30d3afa3b58f2d1cc2
FrancisBaileyH <francisbailey@protonmail.com>
2015-12-03 15:04:03 -0800

    Changed use of dequeueReusableCellWithIdentifier to use more modern approach.

--- Garden Wall/HomeViewController.swift
+++ Garden Wall/HomeViewController.swift
@@ -45,18 +45,13 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        var cell = tableView.dequeueReusableCellWithIdentifier("MenuItemCell") as UITableViewCell?
-        
-        
-        if cell == nil {
-            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "MenuItemCell")
-        }
-        
-        cell!.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
-        cell!.textLabel!.text = menu[indexPath.row]
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("MenuItemCell", forIndexPath: indexPath)
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        cell.textLabel!.text = menu[indexPath.row]
 
         
-        return cell!
+        return cell
     }
     
     
